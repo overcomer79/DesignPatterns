c22720c22dc513439a0cdb82d9c3fa22c9da1447
overcomer <pierpaolo.monaco@gmail.com>
2020-06-04 12:49:05 +0200

    Fix principle

--- Program.cs
+++ Program.cs
@@ -51,6 +51,75 @@ namespace patterns
         }
     }
 
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    /// Combinator
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return this.first.IsSatisfied(t) && this.second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+            {
+                if (spec.IsSatisfied(i))
+                    yield return i;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -66,6 +135,22 @@ namespace patterns
             foreach (var p in pf.FilterByColor(products, Color.Green))
             {
                 Console.WriteLine($" - {p.Name} is green");
+            }
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new): ");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+
+            Console.WriteLine("Large Blue products (new): ");
+            foreach (var p in bf.Filter(products, new AndSpecification<Product>(
+                new ColorSpecification(Color.Blue),
+                new SizeSpecification(Size.Large)))
+                )
+            {
+                Console.WriteLine($" - {p.Name} is big and blue");
             }
 
         }
