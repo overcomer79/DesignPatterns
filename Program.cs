ec5b5a167ab3c81326997b20fb5ae30a504f6263
overcomer <pierpaolo.monaco@gmail.com>
2020-06-04 10:46:11 +0200

    fix pattern

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace patterns
@@ -25,21 +26,28 @@ namespace patterns
             return string.Join(Environment.NewLine, entries);
         }
 
-        public void Save(string fileName)
+
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string fileName, bool overwrite = false)
         {
-            File.WriteAllText(fileName, ToString());
+            if (overwrite || !File.Exists(fileName))
+                File.WriteAllText(fileName, j.ToString());
         }
 
-        public static Journal Load(string fileName)
+        public static Journal Load(Journal j, string fileName)
         {
             throw new NotImplementedException();
         }
 
-        public void Load(Uri uri)
+        public void Load(Journal j, Uri uri)
         {
             throw new NotImplementedException();
         }
     }
+
     class Program
     {
         static void Main(string[] args)
@@ -48,6 +56,10 @@ namespace patterns
             j.AddEntry("Oggi ho iniziato questo corso");
             j.AddEntry("Io amo gli animali");
             Console.WriteLine(j);
+
+            var p = new Persistence();
+            var fileName = @"journal.txt";
+            p.SaveToFile(j, fileName);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # DesignPatterns
 
-SINGLE RESPONSABILITY PRINCIPLE VIOLATED: 
+### SINGLE RESPONSABILITY PRINCIPLE VIOLATED: 
 Added too much responsabilities to the Journal class. 
 (e.g.: It also manages the persistence)
+
+## SINGLE RESPONSABILITY PRINCIPLE RECOVERD
+** A typical class is responsible for one thing and has one reason to change. **
