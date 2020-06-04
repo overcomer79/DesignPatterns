4957ab7862ca0123b42760e0f4e2d7e213ee4644
overcomer <pierpaolo.monaco@gmail.com>
2020-06-04 21:36:59 +0200

    FIx principle violation

--- Program.cs
+++ Program.cs
@@ -2,26 +2,34 @@
 
 namespace patterns
 {
-    
+
     public class Document
     {
 
     }
 
-    public interface IMachine
+    public interface IPrinter
     {
         void Print(Document d);
+    }
+
+    public interface IScanner
+    {
         void Scan(Document d);
+    }
+
+    public interface IFaxer
+    {
         void Fax(Document d);
     }
 
-    public class MultiFunctionPrinter : IMachine
+    public interface IMultiFunctionDevice : IScanner, IPrinter, IFaxer
     {
-        public void Fax(Document d)
-        {
-            //
-        }
 
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
         public void Print(Document d)
         {
             //
@@ -33,30 +41,40 @@ namespace patterns
         }
     }
 
-    // It Only PRINTS !!!!
-    public class OldFashonedPrinter : IMachine
+    public class MultiFunctionMachine : IMultiFunctionDevice
     {
+        private IPrinter printer;
+        private IScanner scanner;
+        private IFaxer faxer;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner, IFaxer faxer)
+        {
+            this.printer = printer ?? throw new ArgumentNullException(paramName: nameof(printer));
+            this.scanner = scanner ?? throw new ArgumentNullException(paramName: nameof(scanner));
+            this.faxer = faxer ?? throw new ArgumentNullException(paramName: nameof(faxer));;
+        }
+
         public void Fax(Document d)
         {
-            throw new NotImplementedException();
+            faxer.Fax(d);
         }
 
         public void Print(Document d)
         {
-            //
+            printer.Print(d);
         }
 
         public void Scan(Document d)
         {
-            throw new NotImplementedException();
-        }
+            scanner.Scan(d);
+        } // decorator pattern
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DesignPatterns
-Your interfaces should be segregated so that nobody who implements your interface has to implement functions which they don't actually need
+Your interfaces should be segregated so that nobody who implements your interface has to implement functions which they don't actually need.
+
+*If you have an interface which includes too much stuff then just break it apart into smaller interfaces.*
