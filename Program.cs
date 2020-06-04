50d8960a944b28e0b2b3836a6fc05f0f701007e2
overcomer <pierpaolo.monaco@gmail.com>
2020-06-04 18:16:45 +0200

    Violated principle

--- Program.cs
+++ Program.cs
@@ -2,11 +2,64 @@
 
 namespace patterns
 {
+    public class Rectangle
+    {
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)} : {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public new int Width
+        {
+            set
+            {
+                base.Width = base.Height = value;
+            }
+        }
+
+        public new int Height
+        {
+            set
+            {
+                base.Height = base.Width = value;
+            }
+        }
+
+    }
+
     class Program
     {
+        static public int Area(Rectangle r) => r.Width * r.Height;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Area(rc)}"); // result 6
+
+            Square sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}"); // result 16
+
+            Rectangle sq2 = new Square();
+            sq2.Width = 4;
+            Console.WriteLine($"{sq2} has area {Area(sq2)}"); // result 0
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DesignPatterns
+
+**Liskov Substitution Principle basically says that you should always be able to sort of upcast**
+The idea is that you should be able to substitute a base type for a subtype.
