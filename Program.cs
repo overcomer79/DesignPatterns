fdb07f7bda10ce408d20005277fab126e31031e1
overcomer <pierpaolo.monaco@gmail.com>
2020-06-04 18:36:50 +0200

    violated principle

--- Program.cs
+++ Program.cs
@@ -2,11 +2,61 @@
 
 namespace patterns
 {
+    
+    public class Document
+    {
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    // It Only PRINTS !!!!
+    public class OldFashonedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DesignPatterns
+Your interfaces should be segregated so that nobody who implements your interface has to implement functions which they don't actually need
